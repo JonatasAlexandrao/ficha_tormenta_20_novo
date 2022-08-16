a6fa69f39c304fbc4d9b995bfe26bc9fca15df21
Jonatas Alexandrao <jonatas-flash@hotmail.com>
2022-08-16 14:41:32 -0300

    refatorando projeto Ficha_Tormenta_20

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import TextBoxCaracter from "./components/TextBoxCaracter/TextBoxCaracter.svelte";
+  import TextBoxDefault from "./components/TextBoxDefault/TextBoxDefault.svelte"
+
+</script>
+
+<main>
+  <div class="wrapper_test">
+    <TextBoxCaracter />
+    <TextBoxDefault nameInput="class-level" text="Jogador" valueLevel=1/>
+  </div>
+</main>
+
+<style>
+  main {
+    
+  }
+
+  .wrapper_test {
+    display: grid;
+    align-items: center;
+
+    position: relative;
+    width: 50vw;
+    height: 50vh;
+
+    background-color: darkcyan;
+    z-index: 1;
+  }
+
+  .teste {
+    background-color: brown;
+    width: 70vw;
+    aspect-ratio: 5/1;
+
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  }
+ 
+</style>
---
+++ src/components/UI/InputDefault/InputDefault.svelte
@@ -0,0 +1,60 @@
+<script>
+
+  export let nameInput = ''
+  export let caracterLength = 8
+  export let className = 'default'
+  export let maxlength = ''
+  export let readonly = false
+
+  export let value = ''
+    $: fontSize = value.length > caracterLength ? calcFontSize() : ''
+  export let valueLevel = ''
+
+  function calcFontSize() {
+    const result = value.length - caracterLength
+
+    if (result >= 6)
+      return 'small'
+    else
+      return `s-${result}`
+  }
+
+  function onKeyPress(e) {
+    if(e.key === 'Enter') {
+      e.target.blur()
+    }
+  }
+
+</script>
+
+<input class="txt_input -{className} {fontSize}" 
+id={nameInput} name={nameInput} bind:value={value}
+maxlength={maxlength}
+on:keypress={onKeyPress}
+readonly={readonly}
+>
+
+{#if nameInput === "class-level"}
+  <input class="txt_input -level" id="level" maxlength="2" bind:value={valueLevel}>
+{/if}
+
+
+<style>
+
+.txt_input {
+  width: 97%;
+  height: 80%;
+  aspect-ratio: 9/1;
+  font-size: 2rem;
+  text-align: center;
+  background-color: var(--color-input);
+  color: inherit;
+  justify-self: center;
+
+}
+
+.txt_input.-character { height: auto; margin: .5% 5% 0; }
+
+.txt_input:focus { outline: 2px solid var(--color-input-selected); }
+  
+</style>
